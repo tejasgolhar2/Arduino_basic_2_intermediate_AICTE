1cc85678874d4c0b7a1173ed11144e8c98f2d2ed
tejasgolhar2 <tejasgolhar2@gmail.com>
2024-06-04 07:27:14 +0530

    commit

---
+++ 0_1_basic/First Arduino Program/1_0_arduino_led_blink1.ino
@@ -0,0 +1,15 @@
+// LED BLINK CODE
+// TinkerCAD Link : https://www.tinkercad.com/things/h6nMJFcmNDB-10arduinoledblink?sharecode=9dLLGcJKxveSBZ3k2KH3uYYrUjqRF6VVV2VpCfSJ8mM
+void setup()
+{
+  	// Select the builtin LED Pin
+	pinMode(13,OUTPUT);	
+}
+
+void loop()
+{
+  	digitalWrite(13,HIGH);	// HIGH --> ON
+  	delay(1000);			// delay of 1 sec
+  	digitalWrite(13,LOW);	// LOW --> OFF	
+  	delay(1000);
+}
